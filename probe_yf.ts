5e682eb8695e791d16b461db0df8123b9b3da4d5
ivanlai33-del <ivanlai.33@gmail.com>
2026-02-27 14:01:42 +0800

    fix: 徹底移除所有舊的獨立測試腳本

--- probe_yf.ts
+++
@@ -1,3 +0,0 @@
-import * as yfModule from 'yahoo-finance2';
-console.log("Keys in yahoo-finance2:", Object.keys(yfModule));
-console.log("Default export keys:", Object.keys((yfModule as any).default || {}));
